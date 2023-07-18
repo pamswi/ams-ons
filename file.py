95485d31419f0567dba6e9b27d9166cd411dd5d0
Pam Swi <pamswiderska@gmail.com>
2023-07-18 11:18:26 +0100

    python exercise added

---
+++ file.py
@@ -0,0 +1,22 @@
+# integers and floats
+number1 = input("enter whole number: ")
+number2 = input("enter decimal number: ")
+
+integer_number = int(number1)
+float_number= float(number2)
+round_number = int(round(float_number))
+
+print(number1)
+print(number2)
+print(round_number)
+
+# boolean
+
+name="Pep Guardogiola"
+age=3
+bark=True
+tweet=False
+
+print("My pet is called", name, ", He is", age, "years old.")
+print("statement:", name, "barks.", bark)
+print("statement:", name, "tweets.", tweet)
