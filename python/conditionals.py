fc373957534c953366b9f0da7a4449f7dbcd1c1b
Pam Swi <pamswiderska@gmail.com>
2023-07-18 14:27:11 +0100

    updated with 3rd alt

--- python/conditionals.py
+++ python/conditionals.py
@@ -11,6 +11,16 @@ if float(mark) <= 65:
     print("fail")
 
 
+# not using elif but nested
+if float(mark) > 65:
+    if float(mark) < 85:
+        print ("pass")
+    else:
+        print ("distinction")
+else:
+    print ("fail")
+
+
 
 # using elif
 if float(mark) > 85:
