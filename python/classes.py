fc7fef3f82c9ae079c612a7c167c902a285713d3
Pam Swi <pamswiderska@gmail.com>
2023-07-24 11:30:37 +0100

    pydoc added into classes.py

--- python/classes.py
+++ python/classes.py
@@ -1,12 +1,18 @@
 # tutorial and exercise
 class Student:
 
-    def __init__(self, name, age, class_name="student"):
+    def __init__(self, name: str, age: int, class_name="student"):
+        '''
+        Student class: will generate a student class 
+        '''
         self.name = name
         self.age = age
         self.class_name = class_name
 
-    def avg_score(self, score1,score2,score3):
+    def avg_score(self, score1: int,score2: int,score3: int) -> int:
+        '''
+        avg_score(scores): will return an average score from data collected
+        '''
         avgs=(score1+score2+score3)/3
         return avgs
 
