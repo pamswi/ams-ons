f27bb6b1860479954357db76780be2c1cf26fcaf
Pam Swi <pamswiderska@gmail.com>
2023-08-02 16:44:24 +0100

    display/positioning & intro to js

---
+++ js/jsintro.js
@@ -0,0 +1,14 @@
+console.log("this is console log");
+
+// variables
+let carMake = "Subaru";
+let carModel = "Impreza";
+// let can be reassigned whereas constant cannot 
+
+console.log("Pam");
+console.info("Swi");
+console.warn("Middle Earth");
+console.error("Elf");
+
+console.log(`My favourite car is %c ${carMake} and the model is ${carModel}`, "font-family:fantasy; color:orange; font-weight:bold; background-color:black; padding: 10px;")
+
